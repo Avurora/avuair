7a5d61d0950cd26e50d865b8ae39645daba0982d
Avurora <58685635+Avurora@users.noreply.github.com>
2020-01-18 22:19:54 +0900

    Update script01.js

--- script01.js
+++ script01.js
@@ -6,12 +6,25 @@ $('#green').click(function(){
     $('#green-list').slideToggle()
 });
 
+$('#red').click(function(){
+    $('#red-list').slideToggle()
+});
 
 
+$('#blue').click(function(){
+    $('#blue-list').slideToggle()
+});
 
 
     
+$('#yellow').click(function(){
+    $('#yellow-list').slideToggle()
+});
+
     
+    $('#purple').click(function(){
+    $('#purple-list').slideToggle()
+});
 
     
     
